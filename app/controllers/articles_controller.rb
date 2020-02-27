293da66ace78f17ed0c7c1169d2185ba674a1216
Oleg Voloshyn <sparco2405@gmail.com>
2020-02-27 18:22:09 +0200

    Add that only singed in user can destroy article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create, :update]
+  before_action :authenticate_user!, :only => [:new, :create, :update, :destroy]
 
   def index
     @articles = Article.all
