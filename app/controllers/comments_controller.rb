54026ba49447697fa1c8e183fca0df3ed5d4d0c9
Oleg Voloshyn <sparco2405@gmail.com>
2020-02-27 17:57:28 +0200

    Made sing in user an author of comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,13 @@
 class CommentsController < ApplicationController
 
-    before_action :authenticate_user!, only => [:create]
+    before_action :authenticate_user!, :only => [:create]
 
 
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    @c = @article.comments.new(comment_params)
+    @c.autor = current_user.username
+    @c.save
     redirect_to article_path(@article)
     
   end
