c767988e4df35f0c288bc5cd38b596bdbee58723
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-13 14:26:36 +0200

    Add validation spec for article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,13 @@ require "rails_helper"
 
 
 describe Article do
-  it {should have_many :comments}
+
+  describe "validation" do #разбили на подгруппы тесты для понимания что тестируем
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :text}
+  end
+
+  describe "assosiations" do
+    it {should have_many :comments}
+  end
 end
