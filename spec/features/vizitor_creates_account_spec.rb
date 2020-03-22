4134ebad97ba2992f48b40e1ff36e4cdb0ff7ca1
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-22 21:44:07 +0200

    Config Rspec for database_cleaner

--- spec/features/vizitor_creates_account_spec.rb
+++ spec/features/vizitor_creates_account_spec.rb
@@ -10,14 +10,6 @@ feature "Account Creation" do
     #не работает тест по кнопке поэтому и не работает exept(page)
 
   end  
-  def sing_up
-    visit new_user_registration_path
-
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'mike123'
-    fill_in :user_password, :with => 'secure123!@#'
-    fill_in :user_password_confirmation, :with => 'secure123!@#'
-    
-  end
+  
   
 end
